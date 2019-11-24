cc7d5f00597170711cec0c8bd139ffe81bc9fcdc
Dominic Gargala <dominic.gargala@hotmail.com>
2019-11-23 22:08:55 -0500

    text

--- text.py
+++ text.py
@@ -18,7 +18,7 @@ def index():
 def sms():
     number = request.form['From']
     message_body = str(request.form['Body'])
-    message_body.split(" ", 1)
+    #message_body.split(" ", 1)
     #resp = MessagingResponse()
     #resp.message('Hello {}, you said: {}'.format("+19056060506", message_body[0]))
     message = client.messages.create(body=message_body[0], from_='+16475576348', to='+19056060506')
