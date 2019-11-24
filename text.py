ca918468b2bd4bd6fd4584c6de9abed57cb22494
Adrian Kalafut <adriankalafut@Adrians-MacBook-Pro.local>
2019-11-24 08:39:35 -0500

    update

--- text.py
+++ text.py
@@ -106,7 +106,7 @@ def sms():
     ##message = client.messages.create(body="Error: You have entered information incorrectly", from_='+16475576348', to= lastClientRequest)
         ##return str(message.sid)
     companiesPresent = 0
-    if(number in userForms.interactions and  message_body[0].isnum()):
+    if(message_body[0].isnum()):
         print("HERE: ", original_message_body )
         compare = linkSkills.get(str(message_body[0]))
         print("Compare: ", compare)
