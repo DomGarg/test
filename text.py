131d72974b8cb585dbe6aee4f5aaa5013047e416
Dominic Gargala <dominic.gargala@hotmail.com>
2019-11-23 22:30:08 -0500

    text

--- text.py
+++ text.py
@@ -17,10 +17,10 @@ def index():
 @app.route('/sms', methods=['POST', 'GET'])
 def sms():
     number = request.form['From']
-    message_body = request.values.get('Body', None).split(" ", 1)
+    message_body = request.values.get('Body', None).split("\n", 1)
     #message_body.split(" ", 1)
     #resp = MessagingResponse()
     #resp.message('Hello {}, you said: {}'.format("+19056060506", message_body[0]))
     message = client.messages.create(body=message_body, from_='+16475576348', to='+19056060506')
     print(message.sid)
-    #return str(resp)
+    return str(message)
