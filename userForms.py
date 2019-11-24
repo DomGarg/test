6d99febb1a064def528abe3309fa3eb7bed3f7f8
Adrian Kalafut <adriankalafut@Adrians-MacBook-Pro.local>
2019-11-24 07:02:56 -0500

    update

--- userForms.py
+++ userForms.py
@@ -17,14 +17,13 @@ class Company():
         ##Update our dictionay of Companies associated with particular skills
         ##Go through the skill(s) the company provided us with and add this Company to each skill they speicliaze within our dictionary
         ##This will be used to reference specific companies when receiving Customer renovation requests
-        
-        temp = Companies.pop(skills)
-        temp.append(self)
-        Companies.update({skills:temp})
         self.skills = skills
         self.phoneNumber = phoneNumber
         self.email = emailAddress
         self.info = companyInfo
+        temp = Companies.pop(skills)
+        temp.append(self)
+        Companies.update({skills:temp})
 
     ##Getter method
     ##Returns the name of the Company associated with this object
