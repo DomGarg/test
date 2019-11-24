7d650b0efff63e983ad78e8b9645b7951b072e4d
Dominic Gargala <dominic.gargala@hotmail.com>
2019-11-23 21:09:12 -0500

    changes

--- text.py
+++ text.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 from twilio import twiml
 from twilio.twiml.messaging_response import MessagingResponse
-
+from twilio.rest import Client
 
 app = Flask(__name__)
+
+account_sid = 'ACb0cf08833e40e8ba91eda2f822201d43'
+auth_token = '4c4a4cffd5e80692cd6b0f74e1027e1b'
+client = Client(account_sid, auth_token)
+
 
 @app.route('/')
 def index():
@@ -14,6 +19,8 @@ def sms():
     number = request.form['From']
     message_body = request.form['Body']
 
-    resp = MessagingResponse()
-    resp.message('Hello {}, you said: {}'.format("+19056060506", message_body))
-    return str(resp)
+    #resp = MessagingResponse()
+    #resp.message('Hello {}, you said: {}'.format("+19056060506", message_body))
+    message = client.messages.create(body="hi", from_='+16475576348', to='+19056060506')
+    print(message.sid)
+    #return str(resp)
